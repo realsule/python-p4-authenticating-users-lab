4b01a2f4db7ea1603b930d1e99ef90d4a78c85b9
Suleiman <real1sule@gmail.com>
2026-01-19 23:43:42 +0300

    code update

--- server/app.py
+++ server/app.py
@@ -48,9 +48,42 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter_by(username=username).first()
+        
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {}, 401
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+        
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            if user:
+                return user.to_dict(), 200
+        
+        return {}, 401
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
